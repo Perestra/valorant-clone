2e1520da32846cc130d306d82557ec137a42a2b4
Perestra <danilo.ps.1996@gmail.com>
2023-03-04 23:17:17 -0300

    Used a prop to receive a backgroundColor on button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './Button.module.scss'
 
-const Button = ({ type, value, title }) => {
+const Button = ({ bgColorBtn, type, value, title }) => {
     return (
         <div className={styles.container_btn}>
-            <button type={type} value={value} className={styles.btn} >
+            <button type={type} value={value} className={styles.btn} style={{ backgroundColor: bgColorBtn }}>
                 <span className={styles.btn__text} >{title}</span>
             </button>
         </div>
