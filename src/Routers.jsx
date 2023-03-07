9ab513e0615c66306eff763d3fb29ac7109bce71
Perestra <danilo.ps.1996@gmail.com>
2023-03-07 16:46:28 -0300

    Added the Footer component in the BrowserRouter

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+
 import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
+
 import Home from 'pages/Home/Home'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -11,6 +14,7 @@ const Routers = () => {
       <Routes>
         <Route index element={ <Home /> }/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
