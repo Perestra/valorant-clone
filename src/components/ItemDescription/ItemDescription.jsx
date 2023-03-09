2274a357186198d5c63a30c90e779fe29ca552ff
Perestra <danilo.ps.1996@gmail.com>
2023-03-08 21:36:42 -0300

    Changed the props name and your values

--- src/components/ItemDescription/ItemDescription.jsx
+++ src/components/ItemDescription/ItemDescription.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
-import Button from 'components/Button/Button'
 import styles from './ItemDescription.module.scss'
+
+import Title from 'components/Title/Title'
+import SubTitle from 'components/SubTitle/SubTitle'
+import Text from 'components/Text/Text'
+import Button from 'components/Button/Button'
+
 import { useNavigate } from 'react-router-dom'
 
-const ItemDescription = ({ h1Class, h1Text, descpritClass, h3Class, h3Text, pClass, pText, video, typeBtn, titleBtn, valueBtn, btnStyle, page }) => {
+const ItemDescription = ({ 
+  titleColor, titleText, 
+  descpritClass, 
+  subTitle, subTitleColor,
+  text, textColor,
+  typeBtn, titleBtn, valueBtn, btnColor, 
+  video, 
+  page }) => {
 
   const navigate = useNavigate()
 
   return (
     <div className={ styles.item }>
       <div className={ styles.item__container }>
-        <h1 className={ h1Class }>{ h1Text }</h1>
+        <Title color={ titleColor } text={ titleText }/>
         <div className={ descpritClass }>
           <div className={ styles.item__description }>
-            <h3 className={ h3Class }>{ h3Text }</h3>
-            <p className={ pClass }>{ pText }</p>
-            <Button type={ typeBtn } title={ titleBtn } value={ valueBtn } btnStyle={ btnStyle } onClick={ () => navigate(page) } />
+            <SubTitle text={ subTitle } color={ subTitleColor } />
+            <Text text={ text } color={ textColor } />
+            <Button type={ typeBtn } title={ titleBtn } value={ valueBtn } color={ btnColor } onClick={ () => navigate(page) } />
           </div>
           { video }
         </div>
