849a437c83f519896270b9b407addcd87072df82
Perestra <danilo.ps.1996@gmail.com>
2023-03-08 21:35:32 -0300

    Added a new color prop to the button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import styles from './Button.module.scss'
 
-const Button = ({ type, value, title, btnStyle, onClick }) => {
+const Button = ({ 
+    onClick, 
+    type, value, color,
+    title }) => {
     return (
-        <div className={styles.container_btn} onClick={ onClick } >
-            <button type={type} value={value} className={styles.btn} style={ btnStyle }>
-                <span className={styles.btn__text} >{title}</span>
+        <div 
+            className={styles.container_btn}            
+            onClick={ onClick } 
+        >
+            <button 
+                type={type} 
+                value={value} 
+                className={
+                   `${styles.btn}
+                    ${styles[color]}`
+                } 
+            >
+                <span className={styles.btn__text}>
+                    {title}
+                </span>
             </button>
         </div>
 
