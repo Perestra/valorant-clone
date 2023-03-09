971f40e20a4324d1c92c15c0919a6e7913aa151c
Perestra <danilo.ps.1996@gmail.com>
2023-03-08 21:37:11 -0300

    Changed some props and its values

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,49 +3,45 @@ import React from 'react'
 import styles from './Home.module.scss'
 
 import { ReactComponent as ValorantText } from 'assets/svg/valorant-text.svg'
+
 import ItemDescription from 'components/ItemDescription/ItemDescription'
 import VideoTag from 'components/VideoTag/VideoTag'
 import ImgTag from 'components/ImgTag/ImgTag'
-import ReynaRaize from 'assets/images/Reyna-Raze.png'
+import ReynaRaize from 'assets/images/home/Reyna-Raze.png'
+import Text from 'components/Text/Text'
 
 const Home = () => {
-
-  const BtnLightColor = {
-    backgroundColor: '#ECE8E1',
-    color: '#383E3A'
-  }
-  const BtnRedColor = {
-    backgroundColor: '#FF4655',
-    color: '#FFFFFF'
-  }
-
   return (
     <main className={ styles.home }>
       <section className={ styles.home__container }>
-        <p>Um jogo de tiro tático 5x5 com personagens originais</p>
+        <Text 
+          text= 'Um jogo de tiro tático 5x5 com personagens originais'
+          color= 'white'
+        />
         <ValorantText className={styles.logo_name}/>
-        <Button type='button' value='Jogue Grátis' title='Jogue Grátis' btnStyle={ BtnRedColor }/>
+        <Button type='button' value='Jogue Grátis' title='Jogue Grátis' color= 'red'/>
       </section>
       <section className={ styles.home__about__section }>
         <ItemDescription 
           descpritClass={ styles.about__description_container }
 
-          h1Text='Nós somos valorant'
-          h1Class={ styles.home__h1About }
+          titleText='Nós somos valorant'
+          titleColor= 'blue'
 
-          h3Text='A criatividade é sua melhor arma.'
-          h3Class={ styles.home__h3About }
+          subTitle='A criatividade é sua melhor arma.'
+          subTitleColor= 'green'
 
-          pText='Misture o seu estilo e experiência em um cenário global e competitivo. 
+          text='Misture o seu estilo e experiência em um cenário global e competitivo. 
           Você terá 13 rodadas para atacar e defender com disparos certeiros e habilidades táticas. 
           Com apenas uma vida por rodada, você deve pensar mais rápido que o oponente se quiser sobreviver. 
           Encare inimigos nos modos Competitivo e Sem Ranque, além da Disputa da Spike e do Mata-Mata!'
-          pClass={ styles.home__pAbout }
+          textColor= 'green'
 
-          titleBtn='Sobre o jogo'
-          valueBtn='Sobre o jogo'
           typeBtn='button'
-          btnStyle={ BtnRedColor }
+          valueBtn='Sobre o jogo'
+          titleBtn='Sobre o jogo'
+          btnColor= 'red'
+          
           page= "/sobre"
           
           video={ <VideoTag /> }
@@ -56,21 +52,22 @@ const Home = () => {
         <ItemDescription 
           descpritClass={ styles.agents__description_container  }
 
-          h1Text='Seus Agentes'
-          h1Class={ styles.home__h1Agents }
+          titleText='Seus Agentes'
+          titleColor= 'white'
 
-          h3Text='Desafie seus limites'
-          h3Class={ styles.home__h3Agents }
+          subTitle='Desafie seus limites'
+          subTitleColor= 'white'
 
-          pText='Mais do que armas e munição, VALORANT inclui agentes com habilidades adaptativas, 
+          text='Mais do que armas e munição, VALORANT inclui agentes com habilidades adaptativas, 
           rápidas e letais, que criam oportunidades para você exibir sua mecânica de tiro.
           Cada Agente é único, assim como os momentos de destaque de cada partida!'
-          pClass={ styles.home__pAgents }
+          textColor= 'white'
 
-          titleBtn='Ver todos os agentes'
-          valueBtn='Ver todos os agentes'
           typeBtn='button'
-          btnStyle={ BtnLightColor }
+          valueBtn='Ver todos os agentes'
+          titleBtn='Ver todos os agentes'
+          btnColor= 'white'
+
           page= "/agentes"
         />
       </section>
@@ -78,25 +75,25 @@ const Home = () => {
         <ItemDescription 
           descpritClass={ styles.maps__description_container  }
 
-          h1Text='Seus Mapas'
-          h1Class={ styles.home__h1Maps }
+          titleText='Seus Mapas'
+          titleColor= 'blue'
 
-          h3Text='Batalhe ao redor do mundo'
-          h3Class={ styles.home__h3Maps }
+          subTitle='Batalhe ao redor do mundo'
+          subTitleColor= 'green'
 
-          pText='Cada mapa serve como um palco para mostrar sua criatividade. 
+          text='Cada mapa serve como um palco para mostrar sua criatividade. 
           Os mapas são feitos sob medida para estratégias de equipe, jogadas espetaculares e momentos eletrizantes. 
           Faça as jogadas que todo mundo vai tentar imitar no futuro!'
-          pClass={ styles.home__pMaps }
+          textColor= 'green'
 
-          titleBtn='Ver todos os mapas'
-          valueBtn='Ver todos os mapas'
           typeBtn='button'
-          btnStyle={ BtnRedColor }
+          valueBtn='Ver todos os mapas'
+          titleBtn='Ver todos os mapas'
+          btnColor= 'red'
+
           page= "/mapas"
         />
       </section>
-      
     </main>
   )
 }
