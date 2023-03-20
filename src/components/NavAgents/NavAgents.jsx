87def540ae6f07deb67ad04f525dd4932eaef303
Perestra <danilo.ps.1996@gmail.com>
2023-03-19 22:12:54 -0300

    Added a new NavAgents

--- src/components/NavAgents/NavAgents.jsx
+++ src/components/NavAgents/NavAgents.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 
+import styles from './NavAgents.module.scss'
+import 'swiper/scss'
+import "swiper/scss/free-mode";
+
 import MenuAgents from 'components/MenuAgents/MenuAgents'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode } from "swiper";
 
 const NavAgents = ({ agents }) => {
   return (
-    <nav>
-      <ul>
-        { agents.map(agent => 
-          <MenuAgents
-            key={ agent.id.toString().padStart(2,'0') }
-            agentId={ agent.id.toString().padStart(2,'0') }
-            agentName={ agent.name }       
-          />
-        ) }
+    <nav className={ styles.nav }>
+      <ul className={ styles.nav__agents } >
+          <Swiper
+            slidesPerView={5.8}
+            freeMode={true}
+            direction="vertical"
+            loop= {true}
+            grabCursor={true}
+            modules={[FreeMode]}
+            className= { styles.nav__agent }
+          >
+            { agents.map(agent => 
+                <SwiperSlide>
+                  <MenuAgents
+                    key={ agent.id.toString().padStart(2,'0') }
+                    agentId={ agent.id.toString().padStart(2,'0') }
+                    agentName={ agent.name }       
+                  />
+                </SwiperSlide>
+            )}
+          </Swiper>
       </ul>
     </nav>
   )
