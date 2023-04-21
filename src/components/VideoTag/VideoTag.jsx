412a4886965124620cf6ee3655e78f47c403d149
Perestra <danilo.ps.1996@gmail.com>
2023-04-21 12:14:40 -0300

    Changed to video tag

--- src/components/VideoTag/VideoTag.jsx
+++ src/components/VideoTag/VideoTag.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
 
 const VideoTag = ({ className, src }) => {
   return (
-    <ReactPlayer 
+    <video
       className={ className } 
-      playing
-      loop={ true }
-      volume= { 1 }
-      muted={ true }
-      width='100%'
-      height='auto'
-      playsinline={ true }
-      url={ src }
-    />
+      src={ src }
+      autoPlay
+      loop
+      muted
+      playsInline
+    >
+    </video>
   )
 }
 
